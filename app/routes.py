d31e3941b4ce04b269f8b060c1cc0f312737513a
Brian <bstanton773@gmail.com>
2022-10-24 16:13:26 -0500

    Adding our first template

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,16 @@
+from flask import render_template
 from app import app
 
 # Create routes for our app
 @app.route('/')
 def index():
-    return 'Hello this is the index'
+    user_info = {
+        'username': 'cbale',
+        'email': 'christianb@movies.com'
+    }
+    return render_template('index.html', user=user_info)
 
 
 @app.route('/posts')
 def posts():
-    return 'Hi this is Posts'
+    return 'Hi this is Posts!'
