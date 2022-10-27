57a241a11f78e9a9ef368b3acccf50a9452999fc
Brian <bstanton773@gmail.com>
2022-10-27 11:17:13 -0500

    Add relationship to User to connect to Post

--- app/models.py
+++ app/models.py
@@ -13,6 +13,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(256), nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
