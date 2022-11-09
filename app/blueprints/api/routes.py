7f1a4875de35a0c6395ff8be361b2ec62fd84516
Brian <bstanton773@gmail.com>
2022-11-09 15:09:33 -0600

    Add to_dict method to Post and then get all posts and get single post endpoint

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -1,6 +1,19 @@
+from flask import jsonify
 from . import api
+from app.models import Post
 
 
 @api.route('/')
 def index():
     return 'Hello this is the API'
+
+
+@api.route('/posts')
+def get_posts():
+    posts = Post.query.all()
+    return jsonify([p.to_dict() for p in posts])
+
+@api.route('/posts/<post_id>')
+def get_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return jsonify(post.to_dict())
